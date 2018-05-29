08374e64efde959c93bb53d7348c993391044a59
145uoka <145uoka@gmail.com>
2018-05-29 11:20:06 +0900

    callback

--- routes/callback.js
+++ routes/callback.js
@@ -1,11 +1,10 @@
 const LINE_CHANNEL_ACCESS_TOKEN = 'NygnlU++I/GLjL2iAuzzZSN608QfhfCDEke8TWjgpobo42Lrc+nl76tRyL/5k7lcVSE6yYasUIz0Bwx3Jc4l/kthxDkDaUS3Zw5HBl7KZG++zRURPQDJxk/hBkHurVYiCZNgP3acAfps4+x4zJinRAdB04t89/1O/w1cDnyilFU=';
 
-var express = require('express');
-var bodyParser = require('body-parser');
-var request = require('request');
-var app = express();
-
 exports.reply = function(req, res, next){
+    var express = require('express');
+    var bodyParser = require('body-parser');
+    var request = require('request');
+    var app = express();
     res.status(200).end();
     for (var event of req.body.events){
         if (event.type == 'message' && event.message.text == 'ハロー'){
