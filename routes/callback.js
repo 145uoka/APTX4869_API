768dfb9022f0a5d2b95b99ab6d20f40f7d7527cc
ito_mo <ito_mo@LAPTOP-HBCTN92J>
2018-05-30 17:37:46 +0900

    tes

--- routes/callback.js
+++ routes/callback.js
@@ -180,30 +180,8 @@ exports.reply = function(req, res, next){
 						json: true
 					});
 
-
-					var http = require("http");
-					var querystring = require("querystring");
-					var StringDecoder = require('string_decoder').StringDecoder;
-					var decoder = new StringDecoder('utf8');
-
-					// 送信データ作成
-						   var jsonData = {
-					           userId:"1",
-					           moneyReceptionFlag:"true",
-					           amount:"190000",
-					           genreId:"1"
-					           }
-					var qs_data = JSON.stringify(jsonData)
-					console.log(qs_data);
-					var options = {
-					    hostname: '192.168.50.5',
-					    port: 8080,
-					    path: '/APTX4869/api/moneyReception/store',
-					    method: 'POST',
-					    headers: {
-					    'Content-Type': 'application/json'
-					    }
-					};
+					var exports_function = require('/routes/money');
+					exports_function.create;
 
 				break;
 
@@ -215,38 +193,6 @@ exports.reply = function(req, res, next){
 			}
 		}
 
-		// リクエスト定義と応答処理設定
-		var apiRes;
-		var req = http.request(options, function(res) {
-		    console.log("STATUS: ", res.statusCode);
-		    console.log("HEADERS: ", JSON.stringify(res.headers));
-		    res.setEncoding('utf8');
 
-		    // 応答受信処理
-		    res.on('data', function(chunk){
-		    	apiRes = chunk;
-		    console.log("BODY: ", chunk);
-		    // Query String -> JSON形式へ変換
-		    var rcv_text = querystring.parse(decoder.write(chunk))
-		        var rcv_json_text = JSON.stringify(rcv_text);
-		        var rcv_json = JSON.parse(rcv_json_text);
-		        console.log("json text = ", rcv_json.message);
-		        console.log("json number = ", rcv_json.sound);
-		        console.log("json boolean = ", rcv_json.reply);
-		    });
-		    // 応答終了処理
-		    res.on('end', function(){
-		    console.log('これ以上データはありません。')
-		    });
-		});
-		// 送信のエラー処理
-		req.on('error', function(e){
-		  console.log( "エラー発生: ", e.message);
-		});
-		// データ送信(GET)
-		req.write(qs_data);
-		req.end();
-
-		res.send("respond create with a resource");
 	}
 };
