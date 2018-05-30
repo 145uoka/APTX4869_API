70fb800c84869830fff513fb4c081eed2c240282
ito_mo <ito_mo@LAPTOP-HBCTN92J>
2018-05-30 17:30:22 +0900

    tes

--- routes/callback.js
+++ routes/callback.js
@@ -180,6 +180,7 @@ exports.reply = function(req, res, next){
 						json: true
 					});
 
+
 					var http = require("http");
 					var querystring = require("querystring");
 					var StringDecoder = require('string_decoder').StringDecoder;
@@ -204,40 +205,6 @@ exports.reply = function(req, res, next){
 					    }
 					};
 
-					// リクエスト定義と応答処理設定
-					var apiRes;
-					var req = http.request(options, function(res) {
-					    console.log("STATUS: ", res.statusCode);
-					    console.log("HEADERS: ", JSON.stringify(res.headers));
-					    res.setEncoding('utf8');
-
-					    // 応答受信処理
-					    res.on('data', function(chunk){
-					    	apiRes = chunk;
-					    console.log("BODY: ", chunk);
-					    // Query String -> JSON形式へ変換
-					    var rcv_text = querystring.parse(decoder.write(chunk))
-					        var rcv_json_text = JSON.stringify(rcv_text);
-					        var rcv_json = JSON.parse(rcv_json_text);
-					        console.log("json text = ", rcv_json.message);
-					        console.log("json number = ", rcv_json.sound);
-					        console.log("json boolean = ", rcv_json.reply);
-					    });
-					    // 応答終了処理
-					    res.on('end', function(){
-					    console.log('これ以上データはありません。')
-					    });
-					});
-					// 送信のエラー処理
-					req.on('error', function(e){
-					  console.log( "エラー発生: ", e.message);
-					});
-					// データ送信(GET)
-					req.write(qs_data);
-					req.end();
-
-					res.send("respond create with a resource");
-
 				break;
 
 			case "2":
@@ -247,5 +214,39 @@ exports.reply = function(req, res, next){
 			default:break;
 			}
 		}
+
+		// リクエスト定義と応答処理設定
+		var apiRes;
+		var req = http.request(options, function(res) {
+		    console.log("STATUS: ", res.statusCode);
+		    console.log("HEADERS: ", JSON.stringify(res.headers));
+		    res.setEncoding('utf8');
+
+		    // 応答受信処理
+		    res.on('data', function(chunk){
+		    	apiRes = chunk;
+		    console.log("BODY: ", chunk);
+		    // Query String -> JSON形式へ変換
+		    var rcv_text = querystring.parse(decoder.write(chunk))
+		        var rcv_json_text = JSON.stringify(rcv_text);
+		        var rcv_json = JSON.parse(rcv_json_text);
+		        console.log("json text = ", rcv_json.message);
+		        console.log("json number = ", rcv_json.sound);
+		        console.log("json boolean = ", rcv_json.reply);
+		    });
+		    // 応答終了処理
+		    res.on('end', function(){
+		    console.log('これ以上データはありません。')
+		    });
+		});
+		// 送信のエラー処理
+		req.on('error', function(e){
+		  console.log( "エラー発生: ", e.message);
+		});
+		// データ送信(GET)
+		req.write(qs_data);
+		req.end();
+
+		res.send("respond create with a resource");
 	}
 };
