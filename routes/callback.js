2e3d231e6bea17d3b827461c918eefc8ecabc94c
ito_mo <ito_mo@LAPTOP-HBCTN92J>
2018-05-31 14:23:12 +0900

    tes

--- routes/callback.js
+++ routes/callback.js
@@ -4,6 +4,10 @@ exports.reply = function(req, res, next){
 	var express = require('express');
 	var bodyParser = require('body-parser');
 	var request = require('request');
+	var http = require("http");
+	var querystring = require("querystring");
+	var StringDecoder = require('string_decoder').StringDecoder;
+	var decoder = new StringDecoder('utf8');
 	var app = express();
 	res.status(200).end();
 	for (var event of req.body.events){
@@ -135,9 +139,28 @@ exports.reply = function(req, res, next){
 
 			var amount = event.message.text;
 
+
 			switch(global.genreMap.get(userId)){
 
 			case "1" :
+
+				var jsonData = {
+					userId:"1",
+					moneyReceptionFlag:"true",
+					amount:"190000",
+					genreId:"1"
+			}
+			var qs_data = JSON.stringify(jsonData)
+			console.log(qs_data);
+			var options = {
+					hostname: '192.168.50.5',
+					port: 8080,
+					path: '/APTX4869/api/moneyReception/store',
+					method: 'POST',
+					headers: {
+						'Content-Type': 'application/json'
+					}
+			};
 
 				// メッセージ設定 「食費：〇〇円」
 				var headers = {
@@ -160,6 +183,40 @@ exports.reply = function(req, res, next){
 					json: true
 				});
 
+				var apiRes;
+				var req = http.request(options, function(res) {
+				    console.log("STATUS: ", res.statusCode);
+				    console.log("HEADERS: ", JSON.stringify(res.headers));
+				    res.setEncoding('utf8');
+
+				    // 応答受信処理
+				    res.on('data', function(chunk){
+				    	apiRes = chunk;
+				    console.log("BODY: ", chunk);
+				    // Query String -> JSON形式へ変換
+				    var rcv_text = querystring.parse(decoder.write(chunk))
+				        var rcv_json_text = JSON.stringify(rcv_text);
+				        var rcv_json = JSON.parse(rcv_json_text);
+//				        console.log("json text = ", rcv_json.message);
+//				        console.log("json number = ", rcv_json.sound);
+//				        console.log("json boolean = ", rcv_json.reply);
+				    });
+				    // 応答終了処理
+				    res.on('end', function(){
+				    console.log('これ以上データはありません。')
+				    });
+				});
+				// 送信のエラー処理
+				req.on('error', function(e){
+				  console.log( "エラー発生: ", e.message);
+				});
+				// データ送信(GET)
+				req.write(qs_data);
+				req.end();
+
+//				var exports_function = require('./money');
+//				exports_function.create('/money/create','./money.create');
+
 				break;
 
 			case "2":break;
@@ -170,7 +227,4 @@ exports.reply = function(req, res, next){
 			}
 		}
 	}
-
-	var exports_function = require('./money');
-	exports_function.create('/money/create','./money.create');
 };
